c567d1f1d8db44ec400f527496f3de6111a91642
KinderjoyStrawberry <kinderjoystrawberry@gmail.com>
2022-06-04 21:00:12 +0700

    move file, add bottom navigation, add 3 main fragment, delete main template

--- app/src/main/java/com/myapp/tourmate/ui/GiveReviewActivity.kt
+++ app/src/main/java/com/myapp/tourmate/ui/GiveReviewActivity.kt
@@ -1,7 +1,8 @@
-package com.myapp.tourmate
+package com.myapp.tourmate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myapp.tourmate.R
 
 class GiveReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/myapp/tourmate/ui/StarterPageActivity.kt
+++ app/src/main/java/com/myapp/tourmate/ui/StarterPageActivity.kt
@@ -1,13 +1,12 @@
-package com.myapp.tourmate
+package com.myapp.tourmate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myapp.tourmate.R
 
-class MainActivity : AppCompatActivity() {
-//    test
-
+class StarterPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_starter_page)
     }
 }
