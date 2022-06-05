f634a446459c5faa4da8855b6c25dcc2f1d4557a
The-RAR <A7118F1480@bangkit.academy>
2022-06-05 21:24:54 +0700

    adding populer places

---
+++ app/src/main/java/com/myapp/tourmate/PopularPlacesActivity.kt
@@ -0,0 +1,11 @@
+package com.myapp.tourmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopularPlacesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_places)
+    }
+}
