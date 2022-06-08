3d901a97b964f34091e32267681bb8b64fb67300
The-RAR <A7118F1480@bangkit.academy>
2022-06-08 21:18:48 +0700

    fixing strterpage to splashscreen

--- app/src/main/java/com/myapp/tourmate/ui/SplashscreenActivity.kt
+++ app/src/main/java/com/myapp/tourmate/ui/SplashscreenActivity.kt
@@ -3,22 +3,21 @@ package com.myapp.tourmate.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.myapp.tourmate.databinding.ActivitySplashscreenBinding
+import android.os.Handler
+import android.os.Looper
+import com.myapp.tourmate.R
 
 class SplashscreenActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySplashscreenBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, delay.toLong())
     }
 
-    private fun btNext() {
-        Intent(this@SplashscreenActivity, LoginActivity::class.java).also {
-            it.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(it)
-        }
+    companion object {
+        const val delay = 3000
     }
 }
