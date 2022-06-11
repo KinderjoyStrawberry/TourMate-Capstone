ea1d896e6bf2a5f5a3f53149604119c119cdcc2d
KinderjoyStrawberry <kinderjoystrawberry@gmail.com>
2022-06-11 20:17:34 +0700

    add change theme setting dark mode

---
+++ app/src/main/java/com/myapp/tourmate/ui/ThemeSettingActivity.kt
@@ -0,0 +1,50 @@
+package com.myapp.tourmate.ui
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.myapp.tourmate.R
+import com.myapp.tourmate.databinding.ActivityThemeSettingBinding
+import com.myapp.tourmate.viewmodels.SettingPreferences
+import com.myapp.tourmate.viewmodels.SettingViewModel
+import com.myapp.tourmate.viewmodels.SettingViewModelFactory
+
+class ThemeSettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThemeSettingBinding
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "setting")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityThemeSettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val switchTheme = binding.btSwitchTheme
+        val pref = SettingPreferences.getInstance(dataStore)
+        val settingViewModel = ViewModelProvider(this, SettingViewModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+        settingViewModel.getThemeSettings().observe(
+            this
+        ) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            settingViewModel.saveThemeSetting(isChecked)
+        }
+    }
+}
