bb4f9d7fe1700d8daaac74043e7830bb24a05234
The-RAR <A7118F1480@bangkit.academy>
2022-06-11 22:50:27 +0700

    fixing review and adding theme

--- app/src/main/java/com/myapp/tourmate/ui/GiveReviewActivity.kt
+++ app/src/main/java/com/myapp/tourmate/ui/GiveReviewActivity.kt
@@ -3,10 +3,16 @@ package com.myapp.tourmate.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.myapp.tourmate.R
+import com.myapp.tourmate.databinding.ActivityGiveReviewBinding
+import com.myapp.tourmate.databinding.ActivityRegisterBinding
 
 class GiveReviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGiveReviewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_give_review)
+        binding = ActivityGiveReviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/myapp/tourmate/ui/ReviewPageActivity.kt
+++ app/src/main/java/com/myapp/tourmate/ui/ReviewPageActivity.kt
@@ -1,12 +1,26 @@
 package com.myapp.tourmate.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.myapp.tourmate.R
+import com.myapp.tourmate.databinding.ActivityGiveReviewBinding
+import com.myapp.tourmate.databinding.ActivityReviewPageBinding
 
 class ReviewPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReviewPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_review_page)
+        binding = ActivityReviewPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnFeedback.setOnClickListener { btGivereview() }
+    }
+
+    private fun btGivereview() {
+        Intent(this@ReviewPageActivity, GiveReviewActivity::class.java).also {
+            startActivity(it)
+        }
     }
 }
