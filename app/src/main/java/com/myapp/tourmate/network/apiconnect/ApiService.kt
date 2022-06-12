cd0e48442e96d2a33a3a4bbe8918860d9acc851e
KinderjoyStrawberry <kinderjoystrawberry@gmail.com>
2022-06-12 13:51:21 +0700

    clean code ApiService

--- app/src/main/java/com/myapp/tourmate/network/apiconnect/ApiService.kt
+++ app/src/main/java/com/myapp/tourmate/network/apiconnect/ApiService.kt
@@ -1,42 +1,14 @@
 package com.myapp.tourmate.network.apiconnect
 
-import androidx.viewbinding.BuildConfig
 import com.myapp.tourmate.network.response.AllPlaceResponse
 import com.myapp.tourmate.network.response.RecomendResponse
 import com.myapp.tourmate.network.response.TourItem
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface ApiService {
-//    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
-//    @GET("search/users")
-//    fun getUsers(
-//        @Query("q") username: String
-//    ): Call<Search>
-//
-//    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
-//    @GET("users/{username}")
-//    suspend fun getUserDetail(
-//        @Path("username") username: String
-//    ): SearchDetail
-//
-//    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
-//    @GET("users/{username}/followers")
-//    fun getFollowers(
-//        @Path("username") username: String?
-//    ): Call<List<ItemsItem>>
-//
-//    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
-//    @GET("users/{username}/following")
-//    fun getFollowing(
-//        @Path("username") username: String?
-//    ): Call<List<ItemsItem>>
-
     @GET("rekomendasi")
-    fun getRecommeded(): Call<RecomendResponse<TourItem>>
+    fun getRecommeded(): Call<RecomendResponse>
 
     @GET("wisata")
     fun getAllPlace(): Call<AllPlaceResponse<TourItem>>
